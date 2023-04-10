4cf23136cfba6ecfd5cc6d73ccec0ad6b2311c0e
sonam23Pathak <sonampathak2001@gmail.com>
2023-04-11 01:06:42 +0530

    ADD: redux for cars

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from "react";
 import logo from "./logo.svg";
 import { Counter } from "./features/counter/Counter";
 import "./App.css";
-import header from "./components/header";
+import Heading from "./components/Heading";
 import Home from "./components/Home";
 import Section from "./components/Section";
 
 function App() {
   return (
     <div className="App">
-      <header />
+      <Heading />
       <Home />
     </div>
   );
--- src/components/header.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function header() {
-  return <div>Header</div>;
-}
-
-export default header;
